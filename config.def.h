d9173274b6cc90ceb2be4f1c3e72eda9855bc4ec
LesenHuang <wongrosen@icloud.com>
2022-12-02 14:25:10 +0800

    modify update

--- config.def.h
+++ config.def.h
@@ -89,8 +89,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = 0  } },
+	{ MODKEY|ControlMask,           XK_minus,  setgaps,        {.i =  0 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = +1  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
